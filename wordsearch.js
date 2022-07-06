c9f2bf238dfc41593e111c2a373470b1b5aec192
Archana Agrawal <archana.agrawal3@outlook.com>
2022-07-06 17:06:13 -0400

    Forgot to refactor in the excitement of everything running soomthly. not sure if I should do it now after pair programming session has ended. so pushing without refactoring.

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,31 @@
+const transpose = function (matrix) {
+
+    const output = [];
+  
+    for (let i = 0; i < matrix.length; i++) {
+      for (let j = 0; j < matrix[i].length; j++) {
+        if (!output[j]) {
+          output[j] =[]
+        }
+        output[j][i] = matrix[i][j] 
+      }
+    }
+    return output;
+  }
+
 const wordSearch = (letters, word) => { 
+    
     const horizontalJoin = letters.map(ls => ls.join(''))
     for (l of horizontalJoin) {
         if (l.includes(word)) return true
-    }
+    } 
+    const switchedArr = transpose(letters);
+    
+    const verticalJoin = switchedArr.map(ls => ls.join(''))
+    for (l of verticalJoin) {
+        if (l.includes(word)) return true
+    }     
+    return false
 }
 
 module.exports = wordSearch
